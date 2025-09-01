1c6ce1916f1fed21a02577b8eb2db1a2b6f11d56
Vishal Mishra <mishrav2@cardiff.ac.uk>
2025-09-01 12:55:55 +0100

    Camera Test

--- carCode/src/cameraTest.py
+++ carCode/src/cameraTest.py
@@ -1,16 +1,12 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Camera na")
-    exit()
-
-ret, frame = cap.read()
-if ret:
-    cv2.imshow("test", frame)
-    cv2.waitKey(0)
-else:
-    print("No frame")
-
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("No Frame")
+        break
+    cv2.imshow("Pi Camera", frame)
+    if cv2.waitKey(1) == ord('q'): break
 cap.release()
 cv2.destroyAllWindows()
