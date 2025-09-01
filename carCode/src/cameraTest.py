c0c8db912064f0582a444dbe6f1a2e02d802ec91
Vishal Mishra <mishrav2@cardiff.ac.uk>
2025-09-01 16:23:07 +0100

    Camera Test

--- carCode/src/cameraTest.py
+++ carCode/src/cameraTest.py
@@ -10,17 +10,24 @@
 #     if cv2.waitKey(1) == ord('q'): break
 # cap.release()
 # cv2.destroyAllWindows()
-from picamera2 import Picamera2
-import cv2
+##########################################################
+# from picamera2 import Picamera2
+# import cv2
 
+# picam2 = Picamera2()
+# picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"}))
+# picam2.start()
+
+# while True:
+#     frame = picam2.capture_array()
+#     cv2.imshow("Pi Cam (Picamera2)", frame)
+#     if cv2.waitKey(1) == ord('q'): break
+
+# cv2.destroyAllWindows()
+# picam2.stop()
+################################################################
+from picamera2 import Picamera2, Preview
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"}))
+picam2.start_preview(Preview.DRM)  # requires a display connected to HDMI
+picam2.configure(picam2.create_preview_configuration())
 picam2.start()
-
-while True:
-    frame = picam2.capture_array()
-    cv2.imshow("Pi Cam (Picamera2)", frame)
-    if cv2.waitKey(1) == ord('q'): break
-
-cv2.destroyAllWindows()
-picam2.stop()
